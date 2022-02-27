9e5f9fb4d15c445b5f905612672c6ea6709f300b
Artur-Ws <wysywysy97@gmail.com>
2022-02-27 20:19:52 +0100

    search_ats method added and getlines method improved in main.py

--- main.py
+++ main.py
@@ -7,17 +7,34 @@ class ABS:
 
         self.abs_file_loc = abs_file_loc
         self.abs_file = open(abs_file_loc)
+        self.list_of_lines = []
 
     def getlines(self):
 
         for line in self.abs_file.readlines():
-            print(line)
-            print("####################")
-            linelist = []
+            char_list = []
             for i in line:
-                linelist.append(i)
-                print(linelist)
+                char_list.append(i)
+            self.list_of_lines.append(char_list)
+
+    def search_ats(self, line):
+        at_indexes =[]
+        for index, char in enumerate(line):
+            if char == "@":
+                at_indexes.append(index)
+        return at_indexes
 
 
+
+ind = []
 abs = ABS(temp1)
 abs.getlines()
+
+# test whether search_ats method works
+for line in abs.list_of_lines:
+    index = abs.search_ats(line)
+    ind.append(index)
+
+for i in ind:
+    print(i)
+
