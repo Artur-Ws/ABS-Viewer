eb009d9420c28ed3e9602c72aad4daa543ebc1a2
Artur-Ws <wysywysy97@gmail.com>
2022-02-28 16:07:44 +0100

    Moved ABS class to new abs.py file, added get value method that get a specific parameter of position and creates a list by datatype. Added BVBS guideline pdf

---
+++ abs.py
@@ -0,0 +1,125 @@
+import numpy as np
+temp1 = "example_abs/1.abs"
+
+
+class ABS:
+    """
+    Create ABS object, that has a methods for .abs files maintenance.
+    It takes path with .abs file name as an input
+    """
+    # DATA TYPES:
+    # • BF2D two dimensional bending shapes
+    # • BF3D three dimensional bending shapes
+    # • BFWE helixes and spirals
+    # • BFMA reinforcing steel meshes
+    # • BFGT lattice girders
+    # • BFAU spacers an supporting cages
+    def __init__(self, abs_file_loc):
+
+        self.abs_file_loc = abs_file_loc
+        self.abs_file = open(abs_file_loc)
+        self.list_of_lines = []
+        self.numbers = []
+        self.bf2d = {}
+        self.bf3d = {}
+        self.bfwe = {}
+        self.bfma = {}
+        self.bfgt = {}
+        self.bfau = {}
+
+        self.getlines()
+
+    def getlines(self):
+        """
+        Appends each line of .abs as a single element to self.list_of_lines list.
+        :return:
+        """
+
+        for line in self.abs_file.readlines():
+            char_list = []
+            for i in line:
+                char_list.append(i)
+            self.list_of_lines.append(char_list)
+
+    def search_ats(self, line):
+        """
+        It will search index of each "@" in given string and append it to at_indexes list.
+        :param line:
+        :return:
+        """
+        at_indexes =[]
+        for index, char in enumerate(line):
+            if char == "@":
+                at_indexes.append(index)
+        return at_indexes
+
+    def get_pos_numbers(self):
+        """
+        Get position numbers and append unique ones to self.numbers list, for all positions in file.
+        :return:
+        """
+        # position number appears after 4th "@" symbol
+        for line in self.list_of_lines:
+            index = self.search_ats(line)
+            self.numbers.append(''.join(line[index[3]+1:index[4]]))
+        return np.unique(self.numbers)
+
+    def get_value(self, at_before, data_type, do_sum=False, delete_first=True):
+        # decide whether or not to skip first sign (if it is a letter before number for example)
+        if delete_first:
+            factor = 2
+        else:
+            factor = 1
+
+        # number of pieces appears after 6th "@" symbol
+
+        unique_positions = self.get_pos_numbers()
+        pos_dict = {}
+        for nr in unique_positions:
+            current_value = 0
+            for line in self.list_of_lines:
+                index = self.search_ats(line)   #list of "@" index
+                if ''.join(line[0:4]) == str(data_type.upper()):
+                    if ''.join(line[index[3]+1:index[4]]) == nr:
+                        if do_sum == True:
+                            print(line[index[at_before]+factor:index[at_before+1]])
+                            current_value += float(''.join(line[index[at_before]+factor:index[at_before+1]]))
+                        elif do_sum == False:
+                            current_value = ''.join(line[index[at_before]+factor:index[at_before+1]])
+            if current_value != 0:
+                pos_dict[nr] = current_value
+        # return pos_dict
+        if data_type == 'bf2d':
+            self.bf2d = pos_dict
+        elif data_type == 'bfma':
+            self.bfma = pos_dict
+
+
+
+
+
+#line[index[first_at]+1:index[last_at]]
+ind = []
+abs = ABS(temp1)
+abs.get_value(5, 'bfma', True, True)
+abs.get_value(5, 'bf2d', True, True)
+x = abs.bfma
+y = abs.bf2d
+print('MESHES:')
+for i in x:
+    print(i, ' number of pieces: ', x[i])
+print('REBARS:')
+for i in y:
+    print(i, ' number of pieces: ', y[i])
+x=[1,2,3,4,5,6,7,8,9]
+
+# print(x[2:5])
+
+# # test whether search_ats method works
+# for line in abs.list_of_lines:
+#     index = abs.search_ats(line)
+#     ind.append(index)
+#
+# for i in ind:
+#     print(i)
+
