118fb6ff48600a124286a23ef107b732c47cbf45
Artur-Ws <wysywysy97@gmail.com>
2022-03-01 22:03:25 +0100

    Added lots of comments and doc strings. First implementation of GUI with tkinter in main.py

--- abs.py
+++ abs.py
@@ -40,7 +40,7 @@ class ABS:
                 char_list.append(i)
             self.list_of_lines.append(char_list)
 
-    def search_symbol(self, line, symbol):
+    def search_symbol_index(self, line, symbol):
         """
         It will search index of each appearance of given symbol in passed (line) and append it to symbol_indexes list.
         :param symbol: symbol which indexes you want to search for
@@ -60,11 +60,26 @@ class ABS:
         """
         # position number appears after 4th "@" symbol
         for line in self.list_of_lines:
-            index = self.search_symbol(line, '@')
-            self.numbers.append(''.join(line[index[3]+1:index[4]]))
+            index = self.search_symbol_index(line, '@')
+            self.numbers.append(''.join(line[index[3]+2:index[4]]))
         return np.unique(self.numbers)
 
     def get_value(self, at_before, data_type, do_sum=False, delete_first=True):
+        """
+        Get values of given parameter passed by 'at_before' and assign it to one of specific datatype lists.\n
+        Possible datatypes:
+            bf2d - 2d shapes,\n
+            bfma - meshes,\n
+            bf3d - 3d shapes,\n
+            bfgt - lattice girders,\n
+            bfwe - helixes and spirals,\n
+            bfau - spacers an supporting cages.
+        :param at_before: value after wchich "@" in a row (starts with "0" value).
+        :param data_type: one of datatype shown above.
+        :param do_sum: warning - pass True only for summable values like number of pieces, or if you want i.e. total mass.
+        :param delete_first: Whether or not to delete first sign of value (it is usually letter irrelevant for value).
+        :return:
+        """
         # decide whether or not to skip first sign (if it is a letter before number for example)
         if delete_first:
             factor = 2
@@ -75,14 +90,18 @@ class ABS:
 
         unique_positions = self.get_pos_numbers()
         pos_dict = {}
+        # for each position in table
         for nr in unique_positions:
             current_value = 0
+            # for each line in .abs file
             for line in self.list_of_lines:
-                index = self.search_symbol(line, '@')   # list of "@" index
+                index = self.search_symbol_index(line, '@')   # list of "@" index
+                # if datatype of that line (first 4 letters) match with given datatype
                 if ''.join(line[0:4]) == str(data_type.upper()):
-                    if ''.join(line[index[3]+1:index[4]]) == nr:
+                    # if position number in table from top loop match position number of actual line
+                    if ''.join(line[index[3]+2:index[4]]) == nr:
                         if do_sum:
-                            print(line[index[at_before]+factor:index[at_before+1]])
+                            #print(line[index[at_before]+factor:index[at_before+1]])
                             current_value += float(''.join(line[index[at_before]+factor:index[at_before+1]]))
                         elif not do_sum:
                             current_value = ''.join(line[index[at_before]+factor:index[at_before+1]])
--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 from abs import ABS
-
+from tkinter import *
 temp1 = "example_abs/1.abs"
 
+abs =ABS(temp1)
+abs.get_value(5, 'bf2d', True)
 
-#line[index[first_at]+1:index[last_at]]
-ind = []
-abs = ABS(temp1)
-abs.get_value(5, 'bfma', True, True)
-abs.get_value(5, 'bf2d', True, True)
-x = abs.bfma
-y = abs.bf2d
-print('MESHES:')
-for i in x:
-    print(i, ' number of pieces: ', x[i])
-print('REBARS:')
-for i in y:
-    print(i, ' number of pieces: ', y[i])
-x = [1,2,3,4,5,6,7,8,9]
 
-# print(x[2:5])
+root = Tk()
+for pos in abs.bf2d:
+    text = str(pos) + "  liczba sztuk: " + str(abs.bf2d[pos])
+    label = Label(root, text=text)
+    label.pack()
 
-# # test whether search_symbol method works
-# for line in abs.list_of_lines:
-#     index = abs.search_symbol(line)
-#     ind.append(index)
+root.mainloop()
+
+# #line[index[first_at]+1:index[last_at]]
+# ind = []
+# abs = ABS(temp1)
+# abs.get_value(5, 'bfma', True, True)
+# abs.get_value(5, 'bf2d', True, True)
+# x = abs.bfma
+# y = abs.bf2d
+# print('MESHES:')
+# for i in x:
+#     print(i, ' number of pieces: ', x[i])
+# print('REBARS:')
+# for i in y:
+#     print(i, ' number of pieces: ', y[i])
+# x = [1,2,3,4,5,6,7,8,9]
 #
-# for i in ind:
-#     print(i)
-
+# # print(x[2:5])
+#
+# # # test whether search_symbol_index method works
+# # for line in abs.list_of_lines:
+# #     index = abs.search_symbol_index(line)
+# #     ind.append(index)
+# #
+# # for i in ind:
+# #     print(i)
+#
