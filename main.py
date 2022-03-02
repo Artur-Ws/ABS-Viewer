06dd7d3d7da5359a1eaab1fa92f60f7676535b0f
Artur-Ws <wysywysy97@gmail.com>
2022-03-02 21:15:00 +0100

    Added meshes display in main.py GUI

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 from abs import ABS
 from tkinter import ttk
 from tkinter import *
-temp1 = "example_abs/1.abs"
+# from natsort import natsorted
+temp1 = "example_abs/2.abs"
 
 abs =ABS(temp1)
 abs.get_value(5, 'bf2d', True)
+abs.get_value(5, 'bfma', True)
 
 
 root = Tk()
+root.title('BVBS-Viewer')
+# root.geometry('300x800')
+
 container = ttk.Frame(root)
 canvas = Canvas(container)
+
 scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
 scrollable_frame = ttk.Frame(canvas)
 scrollable_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
 canvas.configure(yscrollcommand=scrollbar.set)
-for pos in abs.bf2d:
+
+Label(scrollable_frame, text="    PRĘTY\n").pack()
+for pos in sorted(abs.bf2d):
     text = str(pos) + "  liczba sztuk: " + str(abs.bf2d[pos])
     label = Label(scrollable_frame, text=text)
     label.pack()
 
+Label(scrollable_frame, text="\n    SIATKI\n").pack()
+for pos in abs.bfma:
+    text = str(pos) + "  liczba sztuk: " + str(abs.bfma[pos])
+    label = Label(scrollable_frame, text=text)
+    label.pack()
+
 container.pack()
-canvas.pack()
+canvas.pack(side="left", fill="both", expand=True)
 scrollbar.pack(side="right", fill="y")
 root.mainloop()
 
